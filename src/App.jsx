6feb9c3986d8dca1be3185ce902945a40496525d
nikhilmishra <nikhilmishra660@gmail.com>
2025-11-09 16:08:50 +0530

    Initial commit - setup React Tailwind admin dashboard

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import AdminDashboard from './AdminDashboard'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* <AdminDashboard/> */}
+      <div className="bg-blue-500 text-white p-4">
+      <h3>Click me</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
